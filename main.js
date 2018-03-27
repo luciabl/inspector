709b59e784c142c2718c5b9c438da657d8ced401
LBLANC <lucia.blanc@mercadolibre.com>
2018-03-27 15:37:13 -0300

    Changes of events, preserve log and dimensions GA

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ function impresion(id, img, tool, path, type, business, body, body2, link, isVal
 
     if (isValid == 'valid') {
         description = 'Valid Track.'
-        isValid = '<i class="fa fa-circle" style="color:green;" data-toggle="tooltip" title="'+description+'"></i>'
-    }else if (isValid == 'notValid') {
+        isValid = '<i id="circleValid" class="fa fa-circle" style="color:green;" data-toggle="tooltip" title="' + description + '"></i>'
+    } else if (isValid == 'notValid') {
         description = 'Invalid Track: ';
         description += messagesCatalog;
-        isValid = '<i class="fa fa-circle" style="color:red;" data-toggle="tooltip" title="'+description+'"></i>'
-    }else{
+        isValid = '<i id="circleValid" class="fa fa-circle" style="color:red;" data-toggle="tooltip" title="' + description + '"></i>'
+    } else {
         isValid = '';
         description = '';
     }
@@ -33,7 +33,7 @@ function impresion(id, img, tool, path, type, business, body, body2, link, isVal
     } else {
         subbody = '<div class="col-sm-6">' +
             "<h6>Details</h6>" +
-            "<table class='table'>" +
+            "<table class='table' width='100%'>" +
             body +
             "</table>" +
             "</div>" +
@@ -52,7 +52,7 @@ function impresion(id, img, tool, path, type, business, body, body2, link, isVal
             '<div id="imgTool" class="col-sm-1 text-center"><img class="size-img" src="' + img + '"/></div>' +
             '<div id="idValid" class="col-sm-1 text-center">' + isValid + '</div>' +
             '<div id="tool" class="col-sm-2 text-center">' + tool + '</div>' +
-            '<div id="path" class="path col-sm-3 text-center">' + path + '</div>' +
+            '<div id="path" class="path col-sm-3 text-center">' + path + '</textarea></div>' +
             '<div id="type" class="col-sm-2 text-center">' + type + '</div>' +
             '<div id="business" class="col-sm-1 text-center"><img class="size-img" src="' + business + '"/></div>' +
             '<div id="blank" class="col-sm-1"></div>' +
@@ -62,7 +62,7 @@ function impresion(id, img, tool, path, type, business, body, body2, link, isVal
             '</div>' +
             '<div id="' + id + '" class="collapse">' +
             '<div class="card-body">' +
-            '<div class="row">' +
+            '<div class="row" style="overflow: scroll;">' +
             subbody +
             '</div>' +
             '</div>' +
@@ -104,35 +104,34 @@ function llamadas(details) {
 
             formData = join.tracks[j];
 
-            $.ajax(
-            {
-              async: false,
-              url : 'https://api.mercadolibre.com/melidata/catalog/validate',
-              type: "POST",
-              contentType: 'application/json; charset=utf-8',
-              dataType: 'json',
-              data : JSON.stringify(join.tracks[j])
-            })
-            .always(function(data) {
-                path = formData.path;
-                type = formData.type;
-                business = formData.application.business;
-                tool = "MELIDATA";
-                img = "img/md.png";
-                id = formData.id;
-                body += "<div id=" + id + " class=" + id + "></div>";
-                if (data.status == 400){
-                    isValid = 'notValid';   
-                    for (var i = 0; i < data.responseJSON.messages.length; i++) {
-                        messagesCatalog += '\n';
-                        messagesCatalog += '- ' + data.responseJSON.messages[i]
+            $.ajax({
+                    async: false,
+                    url: 'https://api.mercadolibre.com/melidata/catalog/validate',
+                    type: "POST",
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+                    data: JSON.stringify(join.tracks[j])
+                })
+                .always(function(data) {
+                    path = formData.path;
+                    type = formData.type;
+                    business = formData.application.business;
+                    tool = "MELIDATA";
+                    img = "img/md.png";
+                    id = formData.id;
+                    body += "<div id=" + id + " class=" + id + "></div>";
+                    if (data.status == 400) {
+                        isValid = 'notValid';
+                        for (var i = 0; i < data.responseJSON.messages.length; i++) {
+                            messagesCatalog += '\n';
+                            messagesCatalog += '- ' + data.responseJSON.messages[i]
+                        }
+                    } else {
+                        isValid = 'valid'
                     }
-                }else{
-                    isValid = 'valid'             
-                }
-                impresion(id, img, tool, path, type, business, body, body2, link, isValid, messagesCatalog)
-                $("." + id + "").jJsonViewer(JSON.stringify(formData));
-            })
+                    impresion(id, img, tool, path, type, business, body, body2, link, isValid, messagesCatalog)
+                    $("." + id + "").jJsonViewer(JSON.stringify(formData));
+                })
         }
 
     } else if (details.url.match(/\/collect/)) {
@@ -144,8 +143,14 @@ function llamadas(details) {
             sURLVariables = decodeURIComponent(dec.decode(details.requestBody.raw[0].bytes)).split('&');
         }
 
-        path = url.searchParams.get("dp").replace(/\?.*/,"");
+        //path = url.searchParams.get("dp").replace(/\?.*/,"");
         type = url.searchParams.get("t");
+        if (type == 'pageview') {
+            path = url.searchParams.get("dp").replace(/\?.*/, "");
+        } else {
+            path = url.searchParams.get("ec");
+            path += ' - ' + url.searchParams.get("ea")
+        }
         business = url.searchParams.get("dl")
         tool = "GOOGLE ANALYTICS";
         img = "img/ga.png";
@@ -191,9 +196,9 @@ function llamadas(details) {
                 "<td> User Id </td>" +
                 "<td>" + url.searchParams.get("uid") + "</td>" +
                 "</tr>";
-                
 
-            if ((type == 'event')|| (type == 'EVENT')) {
+
+            if ((type == 'event') || (type == 'EVENT')) {
                 body += "<tr>" +
                     "<td><i class='fa fa-folder-open fa-2x'></i></td>" +
                     "<td> Category </td>" +
@@ -216,32 +221,50 @@ function llamadas(details) {
                 "<td> View in GA </td>" +
                 "<td>" + link + "</td>" +
                 "</tr>";
-            
+
             body2 += "<h6>Custom Dimensions</h6>" +
                 "<table class='table'>" +
                 "<th>ID</th><th>Name</th><th>Value</th>";
 
+            var dimensions_id = []
             for (var i = 0; i < sURLVariables.length; i++) {
                 var sParametro = sURLVariables[i].split('=');
                 if (!sParametro[0].indexOf("cd")) {
-                    if (business.match(/mercadoli(b|v)re\./)) {
-                        dimension_name = dimensions["dimensions_ml"][(sParametro[0].substr(2)) - 1].name;
-                        dimension_id = dimensions["dimensions_ml"][(sParametro[0].substr(2)) - 1].id;
-                    } else if (business.match(/www\.mercadopago\./)) {
-                        dimension_name = dimensions["dimensions_mp"][(sParametro[0].substr(2)) - 1].name;
-                        dimension_id = dimensions["dimensions_mp"][(sParametro[0].substr(2)) - 1].id;
-                    } else if (business.match(/developers\.mercadolibre/)) {
-                        dimension_name = dimensions["dimensions_devs"][(sParametro[0].substr(2)) - 1].name;
-                        dimension_id = dimensions["dimensions_devs"][(sParametro[0].substr(2)) - 1].id;
-                    }
-
-                    body2 += "<tr>" +
-                        "<td>" + dimension_id + "</span></td>" +
-                        "<td title='sdfksjhdf'> " + dimension_name + " </td>" +
-                        "<td>" + sParametro[1] + "</td>" +
-                        "</tr>";
+                    dimensions_id.push({id:parseInt(sParametro[0].substr(2)), value:sParametro[1]});
                 }
             }
+
+            dimensions_id.sort(function (a, b) {
+              if (a.id > b.id) {
+                return 1;
+              }
+              if (a.id < b.id) {
+                return -1;
+              }
+              // a must be equal to b
+              return 0;
+            });              
+
+            for (var i = 0; i < dimensions_id.length; i++) {
+                if (business.match(/mercadoli(b|v)re\./)) {
+                    dimension_name = dimensions["dimensions_ml"][(dimensions_id[i].id) - 1].name;
+                    dimension_id = dimensions["dimensions_ml"][(dimensions_id[i].id) - 1].id;
+                } else if (business.match(/www\.mercadopago\./)) {
+                    dimension_name = dimensions["dimensions_mp"][(dimensions_id[i].id) - 1].name;
+                    dimension_id = dimensions["dimensions_mp"][(dimensions_id[i].id) - 1].id;
+                } else if (business.match(/developers\.mercadolibre/)) {
+                    dimension_name = dimensions["dimensions_devs"][(dimensions_id[i].id) - 1].name;
+                    dimension_id = dimensions["dimensions_devs"][(dimensions_id[i].id) - 1].id;
+                }
+
+                body2 += "<tr>" +
+                    "<td>" + dimension_id + "</span></td>" +
+                    "<td title='sdfksjhdf'> " + dimension_name + " </td>" +
+                    "<td>" + dimensions_id[i].value + "</td>" +
+                    "</tr>";  
+            }
+
+
             impresion(id, img, tool, path, type, business, body, body2, link, isValid, messagesCatalog);
         });
     }
@@ -255,17 +278,15 @@ chrome.webRequest.onBeforeRequest.addListener(llamadas, { urls: ["https://*.merc
 
 var consulta = window.matchMedia('(max-width: 767px)');
 
-function mediaQuery(){
-        if (consulta.matches) {
-            console.log("se cumplio");
-            $('.path').attr('class', 'col-sm-10 text-center');
-          }else{
-            console.log("no se cumplio");
-          }  
+function mediaQuery() {
+    if (consulta.matches) {
+        //console.log("se cumplio");
+        $('.path').attr('class', 'col-sm-10 text-center');
+    } else {
+        //console.log("no se cumplio");
+    }
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-  //document.querySelector('button').addEventListener('click', clickHandler);
-  consulta.addListener(mediaQuery);
+document.addEventListener('DOMContentLoaded', function() {
+    consulta.addListener(mediaQuery);
 });
-
